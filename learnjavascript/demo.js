008f2fdb9732c886573825ce8b44a91072fa12bb
dablusingh <35448642+dablusingh@users.noreply.github.com>
2019-03-11 20:19:33 +0530

    projects

---
+++ learnjavascript/demo.js
@@ -0,0 +1,7 @@
+// variables & Datatypes in javascript
+// definition-
+// >>Avariable provies us with a named storage that or  programs can manipulate.
+// >>Variables are used to sotre information to referenced & manipulated
+// in a computer programs.
+// >>In programming a variable is a value that can cahnge,
+// depending on conditions or on information passed to the program.
